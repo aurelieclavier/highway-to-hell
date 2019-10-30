33849e7cc96c4016c5129d7885dfcadf7fab0d3f
aurelie <aurelie.cissokho@gmail.com>
2019-10-30 19:48:43 +0100

    add modal on FilmCard

--- src/components/pageFilm/FilmCard.js
+++ src/components/pageFilm/FilmCard.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 import './FilmCard.css'
 
-const FilmCard = ({title, year, src}) => {
-
-
-
-        return (
-            <div className="cardFilm">
+const FilmCard = (props) => {
+    const {
+        className
+    } = props;
+      const [modal, setModal] = useState(false);
+      const toggle = () => setModal(!modal);
+        
+      return (
+            <div className="cardFilm" onClick={toggle}>
                 <div className="imageFilm">
-                    <img src={src} alt="monster"></img>
+                    <img src={props.src} alt="monster"></img>
                 </div>
                 <div>
                     <hr></hr>
                 </div>
                 <div>
-                    <p>{title}</p>
+                    <p>{props.title}</p>
                 </div>
                 <div>
-                    <p>{year}</p>
+                    <p>{props.year}</p>
                 </div>
-                
+                <div>
+                    <Modal isOpen={modal} toggle={toggle} className={className}>
+                    <ModalHeader toggle={toggle}>{props.title}</ModalHeader>
+                    <ModalBody>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </ModalBody>
+                    <ModalFooter>
+                       
+                    </ModalFooter>
+                    </Modal>
+                </div>
             </div>
         )
     }
