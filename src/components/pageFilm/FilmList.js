ea3b5c4b9a8b74f7d70742ba88b729be278da5d5
Stevie Cutman <talock69@hotmail.com>
2019-10-30 16:45:38 +0100

    requete api dans filmlist

---
+++ src/components/pageFilm/FilmList.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+import FilmCard from './FilmCard'
+import axios from 'axios'
+
+const FilmList = () => {
+
+    const [film, setFilm] = useState([])
+
+
+    const getFilm = () => {
+            axios.get(`https://hackathon-wild-hackoween.herokuapp.com/movies`)
+                .then(response => setFilm(response.data.movies))
+    }
+
+    getFilm();
+
+    console.log(film)
+        return ( 
+            <div>
+                {film.map(x =>(
+                    <FilmCard title={x.title} year={x.year} src={x.posterUrl} />
+                ))}
+            </div>
+         );
+}
+    
+ 
+export default FilmList;
